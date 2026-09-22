997f8e0bd629def4786cfd4974dd43dd7f98076b
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-22 07:45:22 -0500

    feat: User repository add

--- src/main/java/com/tecnil/my_job_api/repository/UserRepository.java
+++ src/main/java/com/tecnil/my_job_api/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.tecnil.my_job_api.repository;
 
 import com.tecnil.my_job_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     public Optional<User> findByUsername(String username);
     public Optional<User> findByPhone(String phone);

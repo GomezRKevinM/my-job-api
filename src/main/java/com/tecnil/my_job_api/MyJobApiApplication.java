9e57501b7247357d726d81d5245548f35ea9fa3c
kgomez <kgomezr2@unicartagena.edu.co>
2026-09-22 07:12:07 -0500

    first commit

---
+++ src/main/java/com/tecnil/my_job_api/MyJobApiApplication.java
@@ -0,0 +1,13 @@
+package com.tecnil.my_job_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJobApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJobApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecnil/my_job_api/MyJobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnil.my_job_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJobApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
